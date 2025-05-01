efc45a18466ae2220e2cc4b6226b58791a963955
008ozriel <peyton.cantabene@me.com>
2025-05-01 11:53:47 -0700

    Changed get_text block to a word count block.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def get_book_text(path_to_file):
+def get_word_count(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-    print("Book text successfully loaded.")
+    text = get_word_count("books/frankenstein.txt")
+    word_count = len(text.split())
+    print(f"{word_count} words found in the document")
 
 main()
